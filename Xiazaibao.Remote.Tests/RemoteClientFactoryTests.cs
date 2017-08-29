51780dfbc877a1afe576c94586c909363c32f9dc
seamys <yiim@foxmail.com>
2017-08-29 13:25:52 +0800

    Initial commit

---
+++ Xiazaibao.Remote.Tests/RemoteClientFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xiazaibao.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiazaibao.Remote.Tests
+{
+  [TestClass()]
+  public class RemoteClientFactoryTests
+  {
+    [TestMethod()]
+    public void GetClientTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
---
+++ Xiazaibao.Remote/RemoteClientFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiazaibao.Remote
+{
+  public class RemoteClientFactory
+  {
+    public IRemoteClient GetClient()
+    {
+      return GetClient(null);
+    }
+
+    public IRemoteClient GetClient(Version version)
+    {
+      return null;
+    }
+  }
+}
